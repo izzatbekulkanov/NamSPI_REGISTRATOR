6397f452a5e08a83afd388c5b9fba37c20a4aabe
Izzatbek Ulkanov <izzatbekulkanov@gmail.com>
2025-05-19 14:12:13 +0500

    sdaa

--- Register/core/urls.py
+++ Register/core/urls.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
 
 urlpatterns = [
-    path('api/', include('api.urls')),  # MUHIM
-    path('admin/', admin.site.urls),
-    path("__reload__/", include("django_browser_reload.urls")),
-    path('', include('queueing.urls')),  # bu default root URL bo‘ladi
-    path('staff/', include('members.urls')),  # bu default root URL bo‘ladi
-    path('registrator/', include('registrator.urls')),  # bu default root URL bo‘ladi
+    path('api/', include('api.urls')),                  # API marshrutlari
+    path('admin/', admin.site.urls),                    # Django admin
+    path('', include('queueing.urls')),                 # Root URL: navbat
+    path('staff/', include('members.urls')),            # Hodimlar uchun login/register
+    path('registrator/', include('registrator.urls')),  # Registrator bo‘limi
 ]
+
+# Faqat DEBUG holatda browser reload yo‘llarini qo‘shish
+if settings.DEBUG:
+    urlpatterns += [
+        path("__reload__/", include("django_browser_reload.urls")),
+    ]
